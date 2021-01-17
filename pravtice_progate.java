4de151f6cf9162525e24590e59e14a28bdcb87af
MUnehiroMachida <life1231@outlook.jp>
2021-01-17 20:49:39 +0900

    足し算

--- pravtice_progate.java
+++ pravtice_progate.java
@@ -3,3 +3,16 @@ class Main {
         System.out.println("Hello World");
     }
 }
+
+// やっぱJavaってClassのなかにメソッド書いて書かないといけないんだ。
+// PHPみたいにちょろっといけないんだな。厳格って事か。
+
+class Main {
+    public static void main(String[] args){
+        System.out.println(17);
+
+        System.out.println(5 + 3);
+
+        System.out.println("5"+"3");
+    }
+}
