5025a3af20a1397e6f6bbf492ad8d8532e9e436b
MUnehiroMachida <life1231@outlook.jp>
2021-01-18 17:35:47 +0900

    か

---
+++ kadai1.java
@@ -0,0 +1,22 @@
+// 問題1
+package example;
+
+public class Mold {
+
+	public static void main(String[] args) {
+
+		String text;
+		text = "テスト";
+		System.out.println(text);
+
+		int number = 100;
+		System.out.println(number);
+
+
+		double  decimal = 1.5;
+		System.out.println(decimal);
+
+		System.out.println(text.length());
+	}
+
+}
